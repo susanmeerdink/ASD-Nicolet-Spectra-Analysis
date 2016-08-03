5e024410e9ad7378c196dfa4b0f0651477e242e6
susanmeerdink <susan.meerdink@gmail.com>
2016-08-03 09:43:05 -0700

    Plotting Spectra by species

--- Nicolet-Spectra-Analysis.R
+++ Nicolet-Spectra-Analysis.R
@@ -10,8 +10,10 @@
 ### Setting Up Environment ###
 library(ggplot2) #load ggplot2 library
 library(moments) #load moments library
+library(reshape2) #load reshape2 library
 
 ### Reading in Data ###
+directory <- "C:\\Users\\Susan\\Documents\\GitHub\\ASD-Nicolet-Spectra-Analysis\\"
 dataFile <- "C:\\Users\\Susan\\Documents\\GitHub\\ASD-Nicolet-Spectra-Analysis\\Nicolet_Averaged_Spectra.csv" #Set to spectra file location
 metaFile <- "C:\\Users\\Susan\\Documents\\GitHub\\ASD-Nicolet-Spectra-Analysis\\Nicolet_Averaged_Spectra_Metadata.csv" #Set to metadata file location
 data <- read.csv(dataFile) #Read in the averaged and std of spectra
@@ -27,12 +29,20 @@ names(std) <- sub("Wavelength","ID",names(std)) #Rename first column to ID inste
 wavelength <- as.numeric(gsub('X','',names(data)[3:length(names(data))])) #Get wavelengths
 
 ### Plotting Spectra ###
-#avgOrder <- avg[order(meta$Acronym),] #Reorder avg spectra based on acronym
 acronym <- unique(meta$Acronym) #Get unique listing of acronyms
 for(a in acronym){
-  plotData <- data.frame(wl = wavelength, t(avg[which(meta$Acronym %in% a),2:ncol(avg)]))
-  ggplot(data = plotData, aes(x = wavelength, y = plotData[,2:ncol(plotData)])) +
-    geom_line()
+  plotData <- data.frame(wl = wavelength, t(avg[which(meta$Acronym %in% a),2:ncol(avg)])) #make current data into data.frame for plotting
+  colnames(plotData) <- c("wl",as.character(avg[which(meta$Acronym %in% a),1])) #rename columns with ID
+  plotDataMelt <-  melt(plotData,id.vars = 'wl') #Melt using wavelength as the ID variable (puts it in format to plot)
+  
+  ggplot(data = plotDataMelt, aes(x = wl,y = value, group = variable,color=variable)) + #Plotting the spectra
+    geom_line()+ #plot as a line
+    xlab(expression(paste("Wavelength (",mu,"m)",sep ="")))+ #add xlabel
+    ylab("Reflectance (%)")+ #add ylabel
+    ggtitle(a) #add title
+  
+  plotName = paste(directory,"Spectra_Plots\\",a,"_spectra.png",sep="") #create plot name to save the file
+  ggsave(plotName,width = 10,height = 5) #save the plot
 }
 
 ### Testing for Normality ###

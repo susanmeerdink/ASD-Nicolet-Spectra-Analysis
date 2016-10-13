8f4679520b96922fc33192f0bf21a0ef7d33fc7f
Susan Meerdink <susan.meerdink@gmail.com>
2016-10-13 11:39:20 -0700

    Adding and Removing Post Hoc Tests

--- Nicolet_Spectra_Analysis_2.R
+++ Nicolet_Spectra_Analysis_2.R
@@ -10,7 +10,8 @@
 ### Setting Up Environment ###
 library(dplyr) #load dplyr for data manipulation 
 library(multcompView) # load library (install if necessary)
-library(DescTools) #load DescTools library for kruskal wallis post test - nemenyi
+library(dunn.test) #load dunn.test library for kruskal wallis post test - dunn 
+library(pgirmess) #load pgirmess library for kruskal wallis post hoc test
 
 ### Reading in Data ###
 directory <- "C:\\Users\\Susan\\Documents\\GitHub\\ASD-Nicolet-Spectra-Analysis\\"
@@ -42,15 +43,14 @@ for(i in 1:nrow(replicationCount)){
 ##Since data is non-parametric, Kruskal-Wallis is need to compare samples
 kwResults <- array(0,dim = c(length(wavelength),2))#Empty array that will hold values below
 kwResults[,1] <- wavelength #add wavelength to the first column
+dunnResults <- array(3,0)#Empty array that will hold values below
 
 fileNameKW <- paste(directory,"Kruskal_Wallis_Test\\kruskal_wallis_test_results.csv",sep="") #create filename that will hold kw test results
 write("Wavelength,KW_pvalue",file = fileNameKW)#write header to file
 fileNameKWPH <- paste(directory,"Kruskal_Wallis_Test\\kruskal_wallis_posthoc_test_results.csv",sep="") #create filename that will hold kw post hoc test results
 write("Wavelength,Pair,Obs-Diff,Critical-Diff,Flag",file = fileNameKWPH)
-fileNameKWPH <- paste(directory,"Kruskal_Wallis_Test\\nemenyi_posthoc_test_results.csv",sep="") #create filename that will hold kw post hoc test results
-write("Wavelength,Pair,Mean_Rank_Diff,P-Value,Flag",file = fileNameKWNE)
-fileNameKWPH <- paste(directory,"Kruskal_Wallis_Test\\mann_whitney_posthoc_test_results.csv",sep="") #create filename that will hold kw post hoc test results
-write("Wavelength,Pair,Obs-Diff,Critical-Diff,Flag",file = fileNameKWMW)
+fileNameKWDT <- paste(directory,"Kruskal_Wallis_Test\\dunn_posthoc_test_results.csv",sep="") #create filename that will hold kw post hoc test results
+write("Wavelength,Pair,P-value",file = fileNameKWDT)
 
 for(x in c(2:length(spectra)) ){ #Loop through wavelengths 
   #All Wavelengths = c(2:length(spectra)) 
@@ -65,7 +65,7 @@ for(x in c(2:length(spectra)) ){ #Loop through wavelengths
   
   ## Post Hoc kruskal-wallis test with pirgimess##
   kwPostTest <- kruskalmc(spectra[,x], meta$Acronym, probs = 0.05) #run post hoc kruskal wallis test 
-  kwPostTestDiff <- kwPostTest[complete.cases(kwPostTest$dif.com),] #Get rid of NaN values
+  kwPostTestDiff <- kwPostTest$dif.com[complete.cases(kwPostTest$dif.com),] #Get rid of NaN values
   pairNames <- row.names(kwPostTestDiff) #Get names of pairs
   
   for(i in c(1:nrow(kwPostTestDiff))){
@@ -74,14 +74,15 @@ for(x in c(2:length(spectra)) ){ #Loop through wavelengths
       write(output,file = fileNameKWPH,append=TRUE,sep = ",") #add line to file 
     }
   }
+
+  ## Post Hoc Kruskal-wallis test with dunn ##
+  kwDunnTest <- dunn.test(x = spectra[,x], g = meta$Acronym)
   
-  ## Post Hoc kruskal-wallis test with nemenyi test ##
-  kwNemenyiTest <- NemenyiTest(x = spectra[,x], g = meta$Acronym)
-  
-  for(i in c(1:nrow(kwNemenyiTest))){
-    if(grepl('TRUE',kwNemenyiTest[i,3]) == TRUE){
-      output <- c(wavelength[x-1],pairNames[i],kwPostTestDiff[i,1],kwPostTestDiff[i,2],kwPostTestDiff[i,3])
-      write(output,file = fileNameKWPH,append=TRUE,sep = ",") #add line to file 
+  for(i in c(1:length(kwDunnTest$P))){
+    if(kwDunnTest$P[i] < 0.05){
+      dunnResults <- rbind(dunnResults,c(wavelength[x-1],kwDunnTest$comparisons[i],kwDunnTest$P[i]))
+      output <- paste(wavelength[x-1],kwDunnTest$comparisons[i],kwDunnTest$P[i],sep = ",")
+      write(output,file = fileNameKWDT,append=TRUE) #add line to file 
     }
   }
 }

2f2f367c7b33f787d13053089aa17f2e8aa5a917
Susan Meerdink <susan.meerdink@gmail.com>
2016-11-02 13:23:45 -0700

    add kruskal wallis and dunn post hoc

--- HyTES_Spectra_Analysis.m
+++ HyTES_Spectra_Analysis.m
@@ -7,18 +7,14 @@
 % spectra process: averaged spectra and standard deviation.
 % This code will analyze the spectra to determine similarities and differences.
 %% Import Data
-directory = 'C:\\Users\\Susan\\Documents\\GitHub\\ASD-Nicolet-Spectra-Analysis\\'; %Home directory
-dataFile = strcat(directory,'HyTES_Spectra_Huntington_33.csv'); %Set to spectra file location
-metaFile = strcat(directory,'HyTES_Spectra_Huntington_Metadata_33.csv'); %Set to metadata file location
+directory = 'F:\\Dropbox\\Analysis\\JPL Analysis\\HyTES ROI spectra\\'; %Home directory
+dataFile = strcat(directory,'HyTES_Spectra_20161101.csv'); %Set to spectra file location
+metaFile = strcat(directory,'HyTES_Metadata_20161101.csv'); %Set to metadata file location
 data = readtable(dataFile); %Read in the averaged and std of spectra
 metaTable = readtable(metaFile); %Read in associated metadata of spectra
 allMeta = table2cell(metaTable); %Convert to cell array
-wavelengths = csvread(dataFile,0,1,[0 1 0 202]); %pull out nicolet wavelengths
-allSpectra = table2cell(data); %convert to cell array
-
-dunnFile = strcat(directory,'\\Kruskal_Wallis_Test\\hytes_dunn_posthoc_test_results.csv');
-dunnWave = readtable(dunnFile); %pull out nicolet wavelengths
-allDunn = table2cell(dunnWave(:,1)); %convert to cell array
+wavelengths = csvread(dataFile,0,1,[0 1 0 202]); %pull out wavelengths
+allSpectra = cell2mat(table2cell(data)); %convert to cell array
 
 %% Find average of species
 acronym = unique(allMeta(:,3));
@@ -42,6 +38,24 @@ for a = 1:length(acronym)
     axis([8 11.6 0.85 1])
     title(acronym(a));
 end
+%% Kruskal Wallis Test and Dunn Post Hoc Test
+pValue = []; %Empty array to hold pvalues from kruskal wallis
+pairs = []; %empty array to hold results from dunn test
+for w = 1:size(wavelengths,2) %loopthrough wavelengths
+    [p,tbl,stats] = kruskalwallis(allSpectra(:,(w+1)),allMeta(:,3),'off');
+    pValue = vertcat(pValue,[wavelengths(w),p]);
+    
+    if p < 0.05 %if that wavelength is statistically significant, do post hoc test
+        c = multcompare(stats,'CType','dunn-sidak','Display','off');
+        for i = 1:size(c,1) %loop through resulting pairs
+            if c(i,6) < 0.05 %if the pair is significantly different add
+                input = [wavelengths(w),stats.gnames(c(i,1)),stats.gnames(c(i,2)),c(i,6)];
+                pairs = vertcat(pairs, input);    
+            end
+        end
+    end
+end
+plot(pValue(:,1),pValue(:,2))
 %% Histogram
 figure('units','normalized','outerposition',[0 0 0.85 1])
 hold on
@@ -51,9 +65,9 @@ ax1 = gca; %current axis
 AxesHandle = findobj(gcf,'Type','axes');
 ax1_pos = get(AxesHandle,'Position'); % position of first axes
 ax2 = axes('Position',ax1_pos,'YAxisLocation','right','Color','none');
-histogram(cell2mat(allDunn),'FaceColor',[0/255 0/255 153/255],'Parent',ax1) %
+histogram(pairs,'FaceColor',[0/255 0/255 153/255],'Parent',ax1) %
 
-input = cell2mat(allSpectra(13,[2:size(allSpectra,2)]));
+input = allSpectra(13,[2:size(allSpectra,2)]);
 line(wavelengths,input,'Color','r','LineWidth',1.5,'Parent',ax2)
 
 set(ax1,'Xlim',[8 11.6],'XTick',[8:.5:12]) %,'Ylim',[0 ],'YTick',[0:0.01: 0.06]

0f2ce00a41fdef6023de9f57e3ee09e1408a9f79
Susan Meerdink <susan.meerdink@gmail.com>
2016-11-02 10:09:30 -0700

    remove f ratio and add histogram

--- Nicolet_Spectra_Analysis.m
+++ Nicolet_Spectra_Analysis.m
@@ -20,30 +20,6 @@ dunnFile = strcat(directory,'\\Kruskal_Wallis_Test\\dunn_posthoc_test_results.cs
 dunnWave = readtable(dunnFile); %pull out nicolet wavelengths
 allDunn = table2cell(dunnWave(:,1)); %convert to cell array
 
-%% F-Ratio
-fratio = zeros(1,length(wavelengths));
-pAll = [];
-wAll =[];
-bAll = [];
-for b = 2: length(wavelengths)
-    [d,p,stats] = manova1(cell2mat(allSpectra(:,b)),allMeta(:,2));
-    fratio(b) = stats.W/stats.B;
-    wAll = vertcat(wAll,stats.W);
-    bAll = vertcat(bAll,stats.B);
-    pAll = vertcat(p, pAll);
-end
-normfratio = 1./fratio; %now low fratio = less important wavelengths
-%% Plot F-Ratio Results
-figure('units','normalized','outerposition',[0 0 1 1])
-hold on
-plot(wavelengths,fratio)
-set(gca,'Xlim',[2.5 15],'Ylim',[0 2],'YTick',[0:.1:1],'XTick',[2.5:2:15]) %
-set(gca,'FontSize',30)
-set(gca,'Fontname','Calibri')
-xlabel(['Wavelength ( \mum )']) % label x-axis
-ylabel('F-ratio') % label left y-axis
-hold off
-
 %% Histogram
 figure('units','normalized','outerposition',[0 0 0.85 1])
 hold on
@@ -65,4 +41,16 @@ ylabel(ax1,'Frequency') % label left y-axis
 ylabel(ax2,'Emissivity') % label left y-axis
 set(ax1, 'FontSize',30)
 set(ax2, 'FontSize',30)
+hold off
+%% Histogram without Emissivity
+figure('units','normalized','outerposition',[0 0 0.85 1])
+hold on
+
+%Create a second axes in the same location as the first axes by setting the position of the second axes equal to the position of the first axes. 
+histogram(cell2mat(allDunn),'FaceColor',[0/255 0/255 153/255]) %
+
+set(gca,'Xlim',[2.5 15],'XTick',[2.5:2:15]) %,'Ylim',[0 ],'YTick',[0:0.01: 0.06]
+xlabel(['Wavelength ( \mum )']) % label x-axis
+ylabel('Frequency') % label left y-axis
+set(gca, 'FontSize',30)
 hold off
